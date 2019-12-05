9fd070a3c8ce152241a6d44d5bfcc4b6bb543f90
K3be <ahmed.13.alkaaby@gmail.com>
2019-12-05 21:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Deciduous extends Tree{
 class Evergreen extends Tree{
   constructor(species,name){
     super(species)
-    this.name = name
+    this.name=name
   }
   static definition(){
     return super.definition() + "Evergreens keep their leaves all year round."
